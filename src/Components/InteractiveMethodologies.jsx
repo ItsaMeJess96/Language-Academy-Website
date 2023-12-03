51f9bfa7301aa181bf2fd66a82aa3522492b3c4b
Jéssica <jessicatavares96@gmail.com>
2023-12-03 19:58:39 +0000

    html metodologias

---
+++ src/Components/InteractiveMethodologies.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from "react";
+
+//informação dos métodos
+const methods = [
+    {
+        title: 'Aulas Presenciais',
+        text: 'As nossas aulas presenciais são ministradas por instrutores altamente qualificados e experientes. Focamos em turmas reduzidas para garantir atenção individualizada',
+        img: 'src/Assets/Icons/alphabet.png',
+        alt: 'quadro de escola com letras'
+    },
+    {
+        title: 'Material Didático',
+        text: 'Utilizamos materiais didáticos modernos e relevantes para cada nível de proficiência. Os livros, recursos online e atividades práticas são cuidadosamente selecionados para enriquecer a experiência de aprendizado',
+        img: 'src/Assets/Icons/books.png',
+        alt: 'icon com livros'
+    },
+    {
+        title: 'Tutorias',
+        text: 'Oferecemos tutorias individuais para alunos que buscam assistência adicional. Os tutores estão disponíveis para esclarecer dúvidas, rever conceitos e fornecer orientação personalizada',
+        img: 'src/Assets/Icons/translator.png',
+        alt: 'pessoa a dizer olá'
+    },
+    {
+        title: 'Integração Multimodal',
+        text: 'Integramos abordagens auditivas, visuais e táteis, criamos um ambiente de apredizagem multimodal. Atividades interativas, vídeos, áudios e jogos são incorporados para tornar as aulas dinâmicas e envolventes',
+        img: 'src/Assets/Icons/puzzle.png',
+        alt: 'puzzle com letras'
+    },
+    {
+        title: 'Avaliações Regulares',
+        text: 'Realizamos avaliações periódicas para acompanhar o progresso dos alunos. É fornecido feedback construtivo para orientar os alunos no aprimoramento contínuo das suas habilidades linguísticas',
+        img: 'src/Assets/Icons/score.png',
+        alt: 'icon de folha exame'
+    },
+    {
+        title: 'Imersão Cultural',
+        text: 'Promovemos experiências de imersão cultural, como eventos temáticos, para aprofundar a compreensão das línguas e culturas estudadas',
+        img: 'src/Assets/Icons/conversation.png',
+        alt: 'pessoas a ter conversa em diferentes línguas'
+    },
+]
+
+function InteractiveMethodologies () {
+
+    //UseState para rastrear o método selecionado, iniciar no primeiro
+    const [currentMethod, setCurrentMethod] = useState(0);
+
+    //Função para atualizar o método a ser mostrado
+    const handleMethodClick = (index) => setCurrentMethod(index);
+
+    return (
+        <div className="m-interactive-methods">
+            <div className="m-method-titles">
+                {methods.map((method, index) => (
+                    <button key={index} onClick={() => handleMethodClick(index)}>
+                        {method.title}
+                    </button>
+                ))}
+            </div>
+            <div className="m-methods-body">
+                <p>{methods[currentMethod].text}</p>
+                <img 
+                    src={methods[currentMethod].img} 
+                    alt={methods[currentMethod].alt} 
+                />
+            </div>
+        </div>
+    )
+};
+
+export default InteractiveMethodologies;
--- src/Pages/Methodologies.jsx
+++ src/Pages/Methodologies.jsx
@@ -1,9 +1,90 @@
+import React from "react";
+//import icons
+import iconTradução from '../Assets/Icons/translation.png'
+//import metodologias interativas
+import InteractiveMethodologies from "../Components/InteractiveMethodologies";
 
 function Methodologies() {
 
     return (
         <div className="methodologies">
-            metodologias
+            <div className="m-intro">
+                <p>
+                    A excelência no ensino é a nossa prioridade! 
+                    Abaixo, encontrarás informações sobre a estrutura do horário das aulas ao longo do ano letivo 
+                    (setembro 2024/junho 2025) e as nossas metodologias de ensino.
+                </p>
+                <img src={iconTradução} alt='icon a representar letra a ser traduzida' />
+            </div>
+            <div className="m-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Seg.</th>
+                            <th>Ter.</th>
+                            <th>Qua.</th>
+                            <th>Qui.</th>
+                            <th>Sex.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <th>08h30 - 10h00</th>
+                            <td>Ingês básico</td>
+                            <td>Português para estrangeiros</td>
+                            <td>Russo básico</td>
+                            <td>Espanhol intermédio</td>
+                            <td>Árabe intermédio</td>
+                        </tr>
+                        <tr>
+                            <th>10h30 - 12h30</th>
+                            <td>Francês básico</td>
+                            <td>Chinês básico</td>
+                            <td>Inglês intermédio</td>
+                            <td>Português para estrangeiros</td>
+                            <td>Russo intermédio</td>
+                        </tr>
+                        <tr>
+                            <th>14h00 - 16h00</th>
+                            <td>Espanhol básico</td>
+                            <td>Japonês básico</td>
+                            <td>Francês intermédio</td>
+                            <td>Chinês intermédio</td>
+                            <td>Inglês avançado</td>
+                        </tr>
+                        <tr>
+                            <th>16h30 - 18h30</th>
+                            <td>Italiano básico</td>
+                            <td>Indi básico</td>
+                            <td>Alemão intermédio</td>
+                            <td>Japonês intermédio</td>
+                            <td>Francês avançado</td>
+                        </tr>
+                        <tr>
+                            <th>19h00 - 21h00</th>
+                            <td>Alemão básico</td>
+                            <td>Árabe básico</td>
+                            <td>Italiano intermédio</td>
+                            <td>Inidi intermédio</td>
+                            <td>Alemão avançado</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="m-methods">
+                <p>
+                    Adotamos uma abordagem abrangente e interativa para garantir que os alunos alcancem fluência e proficiência nas línguas escolhidas.
+                    Eis os principais elementos da nossa metodologia:
+                </p>
+                <InteractiveMethodologies />
+                <p>
+                    Ao escolher a nossa academia optas por uma jornada de apredizagem que vai além da sala de aula, 
+                    proporcionando uma compreensão profunda e holística das línguas que estudas.
+                    Estamos comprometidos em ajudar-te a atingir os teus objetivos linguísticos de maneira eficaz e prazerosa.
+                </p>
+            </div>
         </div>
     )
 };
