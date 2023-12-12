7bd9f41be37b482e6ed9256a4d3030b1634b041a
Jéssica <jessicatavares96@gmail.com>
2023-12-12 14:50:08 +0000

    contacto e install da api dos maps

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,27 @@
+// página de contacto
 
 function Contact() {
     return (
-        <div>Contact Page</div>
+        <div className="contact">
+            <div className="c-message">
+                <h3>Tens alguma dúvida? Entra em contacto connosco!</h3>
+                <from>
+                    <input type="text" name="name" placeholder="nome" />
+                    <input type="email" name="name" placeholder="e-mail" />
+                    <textarea name="message"></textarea>
+                    <input type="submit" value="submeter" />
+                </from>
+            </div>
+            <div className="c-location">
+                <p>Podes encontrar-nos em:</p>
+                <address> Rua da Amostra, 123; 1234-567 Lisboa, Portugal</address>
+                <p>Para mais informações, entra em contacto através do formulário acima ou visita-nos presencialmente</p>
+            </div>
+        </div>
     )
 }
 
 export default Contact;
+
+
+// AIzaSyBuCjayOslvEKH5T74yVawhhU5pEQG8I0s
