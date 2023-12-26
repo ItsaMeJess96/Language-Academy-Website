584cead70ed197652e7259e2d8d438bb1023ee09
Jéssica <jessicatavares96@gmail.com>
2023-12-26 20:35:29 +0000

    hiding api

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -17,7 +17,7 @@ function MapContainer () {
     }
 
     return (
-        <LoadScript googleMapsApiKey="AIzaSyDwyvgiH-400qeQgWBD5daE2VC34siAPQs">
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY}>
             <GoogleMap zoom={14} center={defaultCenter} mapContainerStyle={mapStyle}>
                 <Marker position={defaultCenter} key='marker'/>
             </GoogleMap>
