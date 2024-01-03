f7543b6c303d69a1b11f9b1fd487c7166dd2d949
Jéssica <jessicatavares96@gmail.com>
2024-01-03 17:20:42 +0000

    footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,12 @@
 
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../Styles/Footer.css'
 
 function Footer() {
     return (
         <footer>
-            <div>
+            <div className="footer-main">
                 <div className="adress">
                     <address>
                         Rua da Amostra, 123 <br />
@@ -15,27 +16,25 @@ function Footer() {
                     </address>
                 </div>
                 <div className="social-media">
-                    <i className="fi fi-brands-facebook"></i>
-                    <i className="fi fi-brands-instagram"></i>
-                    <i className="fi fi-brands-linkedin"></i>
+                    <a href='https://www.facebook.com/'><i className="fi fi-brands-facebook"></i></a>
+                    <a href="https://www.instagram.com/"><i className="fi fi-brands-instagram"></i></a>
+                    <a href="https://www.linkedin.com/feed/"><i className="fi fi-brands-linkedin"></i></a>
                 </div>
                 <ul className="policies">
                     <li>
-                        <Link to="/privacyPolicy">Política de Privacidade</Link>
+                        <Link to="/privacyPolicy" className="policy-links">Política de Privacidade</Link>
                     </li>
                     <li>
-                        <Link to="/legalNotice">Aviso Legal</Link>
+                        <Link to="/legalNotice" className="policy-links">Aviso Legal</Link>
                     </li>
                     <li>
-                        <Link to="/cookiesPolicy">Política de Cookie</Link>
+                        <Link to="/cookiesPolicy" className="policy-links">Política de Cookie</Link>
                     </li>
                 </ul>
             </div>
             <div className="referências">
                 <a href="https://www.flaticon.com/uicons">Uicons by Flaticon</a> 
-                <br />
                 <a href="https://www.flaticon.com/free-icons/languages" title="languages icons">Languages icons created by Freepik - Flaticon</a>
-                <br />
                 <a href="https://www.flaticon.com/free-icons/international-flags" title="flags icons">Flags icons created by Freepik - Flaticon</a>
             </div>
 
