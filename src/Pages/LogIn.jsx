4490beb344a109d1f91caebac87dd2ab31fc91e9
Jéssica <jessicatavares96@gmail.com>
2024-01-09 15:00:39 +0000

    css logIn e SignUp

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,5 +1,6 @@
 // página em que o usuário faz log in
 
+import '../Styles/LogIn&SignUp.css'
 import iconScrabble from '../Assets/Icons/scrabble.png'
 import { Link } from 'react-router-dom';
 
@@ -7,14 +8,16 @@ function LogIn() {
     return (
         <div className="log-in">
             <div className='l-intro'>
-                <h3>Inicio de Sessão</h3>
-                <p>
-                    É bom ter-te de volta!
-                    Faz o log in para continuares a tua experiência de aprendizagem.
-                </p>
+                <div>
+                    <h3>Inicio de Sessão</h3>
+                    <p>
+                        É bom ter-te de volta!
+                        Faz o log in para continuares a tua experiência de aprendizagem.
+                    </p>
+                </div>
             </div>
-            <div className='l-form'>
-                <form>
+            <div  className='l-form-container'>
+                <form className='l-form'>
                     <label htmlFor='username'>Nome de utilizador:</label>
                     <input type='text' id='s-username' name='username' required />
                     <label htmlFor='email'>Email:</label>
@@ -23,12 +26,12 @@ function LogIn() {
                     <input type='password' id='s-password' name='password' required />
                     <input type="submit" value='Entrar' />
                 </form>
-                <img src={iconScrabble} alt="scrabble a mostrar a palavra learn" />
             </div>
             <div className='l-sign'>
+                <img id='icon-logIn' src={iconScrabble} alt="scrabble a mostrar a palavra learn" />
                 <p>
                     Ainda não tens uma conta?  
-                    <Link to='/signup'> Regista-te aqui! </Link>
+                    <Link to='/signup' id='logIn-link'> Regista-te aqui! </Link>
                 </p>
             </div>
         </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,19 +1,23 @@
 // página em que o usuário se pode inscrever na plataforma
 
+import '../Styles/LogIn&SignUp.css'
 import iconPC from '../Assets/Icons/online-lesson2.png'
 
 function SignUp() {
     return (
         <div className="sign-up">
             <div className='s-intro'>
-                <h3>Resgista-te na nossa plataforma!</h3>
-                <p>
-                    Damos-te as boas vindas à Culturália!
-                    Regista-te para começares a tua jornada de aprendizagem de línguas.
-                </p>
+                <div>
+                    <h3>Resgista-te na nossa plataforma!</h3>
+                    <p>
+                        Damos-te as boas vindas à Culturália!
+                        Regista-te para começares a tua jornada de aprendizagem de línguas.
+                    </p>
+                </div>
+                <img id='icon-signUp' src={iconPC} alt="ecrã a mostrar aula online" />
             </div>
-            <div className='s-form'>
-                <form>
+            <div className='s-form-container'>
+                <form  className='s-form'>
                     <label htmlFor='username'>Nome de utilizador:</label>
                     <input type='text' id='s-username' name='username' required />
                     <label htmlFor='email'>Email:</label>
@@ -24,7 +28,6 @@ function SignUp() {
                     <input type='password' id='s-password-repeat' name='password-repeat' required />
                     <input type="submit" value='Registar' />
                 </form>
-                <img src={iconPC} alt="ecrã a mostrar aula online" />
             </div>
         </div>
     )
