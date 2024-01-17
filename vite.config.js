52da15a097380181fbf0f0e5ea2a344448421288
Jéssica <jessicatavares96@gmail.com>
2024-01-17 15:30:07 +0000

    updates and alterations to work on cyclic

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log('Vite is starting...');
+
 export default defineConfig({
   plugins: [react()],
   server: {
