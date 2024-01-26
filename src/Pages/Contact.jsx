f3026389fcf51afaf3a1e353c2835c34721fc1b2
Jéssica <jessicatavares96@gmail.com>
2024-01-26 20:42:15 +0000

    final configurations

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,56 +1,19 @@
 // página de contacto
 
 import '../Styles/Contact.css'
-import { useState } from "react";
 //import do mapa
 import MapContainer from "../Components/Map";
 
 function Contact() {
-
-    // iniciar o useState
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: '',
-    });
-
-    // função a ser chamada quando ocorre uma mudança
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    /* função a ser chamada quando o formulário é enviado
-     previne o comportamento padrão e faz solicitação POST para o <servidor></servidor> */
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch('http://localhost:3000/send-email', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(formData),
-            });
-            if (response.ok) {
-                console.log('Email enviado com sucesso!');
-            } else {
-                console.error('Erro ao enviar o email.');
-            }
-        } catch (error) {
-            console.error('Erro inesperado: ', error)
-        }
-    };
-
-
-
     return (
         <div className="contact">
             <div className="c-message">
                 <h3>Tens alguma dúvida?</h3>
                 <h3>Entra em contacto connosco!</h3>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="name" placeholder="nome" id="c-name" autoComplete='name' required onChange={handleChange} />
-                    <input type="email" name="email" placeholder="e-mail" id="c-email" autoComplete='email' required onChange={handleChange} />
-                    <textarea name="message" id="c-message" required onChange={handleChange}></textarea>
+                <form>
+                    <input type="text" name="name" placeholder="nome" id="c-name" autoComplete='name' required />
+                    <input type="email" name="email" placeholder="e-mail" id="c-email" autoComplete='email' required />
+                    <textarea name="message" id="c-message" required ></textarea>
                     <input type="submit" value="submeter" />
                 </form>
             </div>
